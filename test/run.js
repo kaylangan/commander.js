a4ca1d4cae6ca9a9ed62fbc651c0ec44ca1b042c
Kayla Ngan <kangan@microsoft.com>
2019-01-18 11:50:29 -0500

    Skip signal tests on Windows since signals are unsupported

--- test/run.js
+++ test/run.js
@@ -12,7 +12,10 @@ readdirSync(__dirname).forEach((file) => {
     return;
   process.stdout.write(`\x1b[90m   ${file}\x1b[0m `);
   const result = spawnSync(process.argv0, [ join('test', file) ]);
-  if (result.status === 0) {
+  if (result.stdout == 'SKIP ON WINDOWS') {
+    process.stdout.write('\x1b[36mSKIP ON WINDOWS\x1b[0m\n');
+  }
+  else if (result.status === 0) {
     process.stdout.write('\x1b[36m✓\x1b[0m\n');
   } else {
     process.stdout.write('\x1b[31m✖\x1b[0m\n');
--- test/test.command.executableSubcommand.signals.hup.js
+++ test/test.command.executableSubcommand.signals.hup.js
@@ -6,17 +6,23 @@ var bin = path.join(__dirname, './fixtures/pm');
 var proc = spawn(bin, ['listen'], {});
 
 var output = '';
-proc.stdout.on('data', function (data) {
-  output += data.toString();
-});
-
-// Set a timeout to give 'proc' time to setup completely
-setTimeout(function () {
-  proc.kill('SIGHUP');
-
-  // Set another timeout to give 'prog' time to handle the signal
-  setTimeout(function() {
-    output.should.equal('SIGHUP\n');
-  }, 1000);
-
-}, 2000);
+// Skip this test for Windows since signals are unsupported
+if (process.platform === 'win32') {
+  process.stdout.write('SKIP ON WINDOWS');
+  return;
+} else {
+  proc.stdout.on('data', function (data) {
+    output += data.toString();
+  });
+  
+  // Set a timeout to give 'proc' time to setup completely
+  setTimeout(function () {
+    proc.kill('SIGHUP');
+  
+    // Set another timeout to give 'prog' time to handle the signal
+    setTimeout(function() {
+      output.should.equal('SIGHUP\n');
+    }, 1000);
+  
+  }, 2000);
+}
